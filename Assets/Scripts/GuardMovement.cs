e2e5ddc659a2fbf532359267ad7f43ac55d4e670
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2023-07-07 22:10:35 +0200

    first iteration of the guard movement

--- Assets/Scripts/GuardMovement.cs
+++ Assets/Scripts/GuardMovement.cs
@@ -1,29 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GuardMovement : MonoBehaviour
 {
 
-    public List<Transform> positions = new List<Transform>();
+    // Store reference to the waypoint system
+    [SerializeField] private Waypoints waypoints;
 
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    [SerializeField] private float distanceTreshold = 5f;
+
+
+    private Transform currentWaypoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (positions.Count == 0)
+        if (waypoints == null)
         {
             Debug.LogError("No Positions given to gaurd: " + gameObject.name);
             return;
         }
 
-        transform.position = positions[0].position;
+        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+
+        transform.position = currentWaypoint.position;
+
+        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+        transform.LookAt(currentWaypoint);
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceTreshold)
+        {
+            currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+        }
     }
 }
